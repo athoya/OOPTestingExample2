d470e588f1564b3784d5ae2566c90f20417cb514
athoya <athoyaa@gmail.com>
2019-05-15 13:24:31 +0700

    Initial Commit

---
+++ OOPTestingExample2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOPTestingExample2
+//
+//  Created by Jazilul Athoya on 15/05/19.
+//  Copyright © 2019 Jazilul Athoya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
